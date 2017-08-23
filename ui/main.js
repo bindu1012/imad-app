d378458856df94538feca296d401441396d95c94
bindu1012 <bbhargavi1012@gmail.com>
2017-08-24 00:05:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,19 @@ button.onclick = function () {
 request.open('GET', 'http://bbhargavi1012.imad.hasura-app.io/counter', true );
 request.send(null);
 };
+
+//submit name
+var nameinput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    //made a request to the server and send the name
+    //capture the list of name and render it as a list
+    var names = ['name1', 'name2', 'name3', 'name4'];
+    var list = '';
+    for(var i=0; i<names.length; i++){
+        list += '<li>'+names[1]+ '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ui.innerHTML = list;
+};
