8fa38343d0655c0640aca299601e486ab06c42d7
bindu1012 <bbhargavi1012@gmail.com>
2017-08-24 00:11:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,7 @@ request.send(null);
 };
 
 //submit name
-var nameinput = document.getElementById('name');
+var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
@@ -31,7 +31,7 @@ submit.onclick = function () {
     //capture the list of name and render it as a list
     var names = ['name1', 'name2', 'name3', 'name4'];
     var list = '';
-    for(var i=0; i<names.length; i++){
+    for(var i=0; i< names.length; i++){
         list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
