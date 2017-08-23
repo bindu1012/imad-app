8de980667521e6650aec8ffe1f846552da437127
bindu1012 <bbhargavi1012@gmail.com>
2017-08-23 14:50:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ var element=document.getElementById('main-text');
 element.innerHTML='new value';
 //move the image
 var img=document.getElementById('madi');
-img.onclick= function () {
+img.onclick = function () {
 img.style.marginLeft = '100px';  
 };
