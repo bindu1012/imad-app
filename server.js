9bb8c8013557b38c6019d7b9ddf1d1b659239220
bindu1012 <bbhargavi1012@gmail.com>
2017-08-31 01:49:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ app.get('/articles/:articleName', function (req,res) {
     //it means articleName==article-one
     //articles[articleName]={} content object for article one
     //SELECT * FROM article WHERE title='article-one'
-    pool.query("SELECT * FROM TABLE article WHERE title = '" + req.prams.articleName + "'", function (err, result) {
+    pool.query("SELECT * FROM TABLE article WHERE title = '" + req.params.articleName + "'", function (err, result) {
       if(err) {
           res.status(500).send(err.toString());
       } else {
